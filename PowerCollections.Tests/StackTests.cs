fdb8698eb5f8dbd3f57faeaf4275d942fbd2b22b
koneneru <imarikkard@gmail.com>
2023-03-13 11:54:14 +0400

    Added artifacts

---
+++ PowerCollections.Tests/StackTests.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Wintellect.PowerCollections.Tests
+{
+    [TestClass]
+    public class StackTests
+    {
+        //Constuctor Test
+        [TestMethod]
+        public void Constructor_ShouldCreateStackWithCorrectCapacity()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            Assert.AreEqual(3, stack.Capacity);
+        }
+
+        //Count Test
+        [TestMethod]
+        public void Count_ShouldCorrectlyDisplayTheNumberOfElementsOnStack()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        //IsEmpty Tests
+        [TestMethod]
+        public void IsEmty_ShouldBeTrueWhenStackIsNew()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [TestMethod]
+        public void IsEmpty_ShouldBeFalseAfterPush()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(3);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [TestMethod]
+        public void IsEmpty_ShouldBeTrueAfterPop()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(1);
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        //IsFull Tests
+        [TestMethod]
+        public void IsFull_ShouldBeFalseWhenStackIsNew()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            Assert.IsFalse(stack.IsFull);
+        }
+
+        [TestMethod]
+        public void IsFull_ShouldBeTrueAfterFilling()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Push(1);
+            Assert.IsTrue(stack.IsFull);
+        }
+
+        [TestMethod]
+        public void IsFull_ShouldBeFalseAfterPop()
+        {
+            Stack<int> stack = new Stack<int>(2);
+            stack.Push(2);
+            stack.Push(1);
+            stack.Pop();
+            Assert.IsFalse(stack.IsFull);
+        }
+
+        //Push Tests
+        [TestMethod]
+        public void Push_ShouldBeOKWhenStackIsNotFull()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(3);
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Push_ShouldThrowExceptionWhenStackIsFull()
+        {
+            Stack<int> stack = new Stack<int>(1);
+            stack.Push(3);
+            stack.Push(2);
+        }
+
+        //Pop Tests
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Pop_ShouldThrowExceptionWhenStackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Pop();
+        }
+
+        [TestMethod]
+        public void Pop_ShouldBeOKWhenSatckIsNotEmpty()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(3);
+            stack.Pop();
+            Assert.AreEqual(0, stack.Count);
+        }
+
+        //Top Tests
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Top_ShouldThrowExceptionWhenStackIsEmpty()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Top();
+        }
+
+        [TestMethod]
+        public void Top_ShouldBeOKWhenStackIsNotEmpty()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(3);
+            stack.Top();
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        //Enumerator Test
+        [TestMethod]
+        public void Enumerator_ShouldReturnStackFromTopToBottom()
+        {
+            Stack<int> stack = new Stack<int>(3);
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            int[] refArr = new int[] { 3, 2, 1 };
+            int[] testArr = new int[3];
+            int i = 0;
+            foreach (int item in stack)
+                testArr[i++] = item;
+            CollectionAssert.AreEqual(refArr, testArr);
+        }
+    }
+}
+
---
+++ PowerCollections/Stack.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Wintellect.PowerCollections
+{
+    public class Stack<T> : IEnumerable<T>
+    {
+        private readonly T[] _memory;
+        private int index;
+
+        public int Count
+        {
+            get { return index + 1; }
+        }
+        public int Capacity
+        {
+            get { return _memory.Length; }
+        }
+
+        public bool IsEmpty
+        {
+            get { return Count == 0; }
+        }
+
+        public bool IsFull
+        {
+            get { return Count == Capacity; }
+        }
+
+        public Stack(int size = 100)
+        {
+            _memory = new T[size];
+            index = -1;
+        }
+
+        public void Push(T item)
+        {
+            if (IsFull)
+                throw new InvalidOperationException("Stack overflow");
+            _memory[++index] = item;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+            return _memory[index--];
+        }
+
+        public T Top()
+        {
+            if (IsEmpty)
+                throw new InvalidOperationException("Stack is empty");
+            return _memory[Count];
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (IEnumerator<T>)GetEnumerator();
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new StackEnumerator<T>(_memory, Count);
+        }
+    }
+
+    class StackEnumerator<T> : IEnumerator<T>
+    {
+        private readonly T[] array;
+        private int position;
+        private readonly int capasity;
+        public T Current
+        {
+            get
+            {
+                return array[position];
+            }
+        }
+        object IEnumerator.Current => Current!;
+
+        public StackEnumerator(T[] array, int capasity)
+        {
+            this.array = array;
+            this.capasity = capasity;
+            position = capasity;
+        }
+
+        public bool MoveNext()
+        {
+            position--;
+            return position > -1;
+        }
+
+        public void Reset() => position = capasity;
+        public void Dispose() { }
+    }
+}
